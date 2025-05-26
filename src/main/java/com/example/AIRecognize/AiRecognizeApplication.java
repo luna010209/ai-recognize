1d961b69354820da2f049b1dd4b2445e5db7af4d
Lunado <liendhhha140217@gmail.com>
2025-05-26 23:43:44 +0900

    Initialize project

---
+++ src/main/java/com/example/AIRecognize/AiRecognizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AIRecognize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecognizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRecognizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AIRecognize/AiRecognizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIRecognize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecognizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
